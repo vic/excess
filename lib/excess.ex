1fab284468cb9afbd6924391e9de90f1669b23e2
Victor Hugo Borja <vborja@apache.org>
2017-07-27 13:38:23 -0500

    empty

---
+++ lib/excess.ex
@@ -0,0 +1,18 @@
+defmodule Excess do
+  @moduledoc """
+  Documentation for Excess.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Excess.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/excess_test.exs
@@ -0,0 +1,8 @@
+defmodule ExcessTest do
+  use ExUnit.Case
+  doctest Excess
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
